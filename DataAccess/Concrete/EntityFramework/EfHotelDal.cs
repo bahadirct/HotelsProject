9c6603c08e55d405907d7dc53c3c45ee4117ad48
Bahadır can Topçu <bahadircantopcu@gmail.com>
2022-07-07 14:51:51 +0300

    Test amaçlı InMemory yönetimini kullanıyordum burada yeni bir veri erişim yöntemi ekledim (Entity Framework.)

---
+++ DataAccess/Concrete/EntityFramework/EfHotelDal.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    internal class EfHotelDal : IHotelDal
+    {
+        public void Add(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Hotel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Hotel> GetAllByStar(int star)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
