a9b117d956ef9b3a96b9ff3051dad041994b1c46
Bahadır can Topçu <bahadircantopcu@gmail.com>
2022-07-07 16:23:33 +0300

    Varlıklar için farklı farklı işlem arayüzleri oluşturmak yerine hepsi için geçerli bir genel işlem deposu arayüzü oluşturdum. GetAll Komutunu filtreler ile birlikte kullanmak için güncelledim ve tek bir kayıt getirebilmek için yine filtrelerle birlikte kullanabildiğim Get metodunu ekledim.

--- DataAccess/Abstract/IHotelDal.cs
+++ DataAccess/Abstract/IHotelDal.cs
@@ -7,13 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IHotelDal
+    public interface IHotelDal:IEntityRepository<Hotel>
     {
-        List<Hotel> GetAll();
-        void Add(Hotel hotel);
-        void Update(Hotel hotel);
-        void Delete(Hotel hotel);
-
-        List<Hotel> GetAllByStar(int star);
+       
     }
 }
