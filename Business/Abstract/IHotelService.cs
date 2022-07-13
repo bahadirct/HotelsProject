89ccf5288213949e3cc933ef07e4ffd9316948bb
Bahadır can Topçu <bahadircantopcu@gmail.com>
2022-07-14 01:07:28 +0300

    Update - HotelManager (GetDbFile, WriteAsXml, WriteAsJson, Add, AddRange) - 'GetDbFile' sınıfı içinde artık hem dosayayı çekip root'a yazdırma hem çekilen dosyayı json tipine çevirme hem de json tipine çevirilen dosyadaki verileri veri tabanına ekleme (AddRange) işlemleri yapılıyor. - Verileri Json ve Xml tipinde dışarı aktarmak için metodlar oluşturdum (Boş)

--- Business/Abstract/IHotelService.cs
+++ Business/Abstract/IHotelService.cs
@@ -1,5 +1,7 @@
 ﻿using Core.Utilities.Results;
 using Entities.Concrete;
+using Entities.Views;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,14 @@ namespace Business.Abstract
 {
     public interface IHotelService
     {
+        IResult Add(Hotel hotel);
+        IResult AddRange(IEnumerable<Hotel> hotel);
         IDataResult<List<Hotel>> GetAll();
-        IDataResult<Hotel> GetById(int id);
+        IDataResult<Hotel> GetByName(string name);
         IDataResult<Hotel> GetAllByStar(int star);
-
+        IResult WriteAsJson(string path, List<Hotel> hotels);
+        IResult WriteAsXml(string path, List<Hotel> hotels);
+        IResult GetDbFile(IFormFile file);
 
     }
 }
--- Business/Concrete/HotelManager.cs
+++ Business/Concrete/HotelManager.cs
@@ -1,11 +1,18 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Core.Utilities.Results;
+using CsvHelper;
+using CsvHelper.Configuration;
 using DataAccess.Abstract;
 using DataAccess.Concrete;
 using Entities.Concrete;
+using Entities.Views;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +22,7 @@ namespace Business.Concrete
     public class HotelManager : IHotelService
     {
         private readonly IHotelDal _hotelDal;
+        static string directory = "wwwroot";
 
         public HotelManager(IHotelDal hotelDal)
         {
@@ -23,6 +31,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Hotel>> GetAll()
         {
+            //JsonConvert.SerializeObject(GELENDATA)
             return new SuccessDataResult<List<Hotel>>(_hotelDal.GetAll(),Messages.HotelList);
         }
 
@@ -31,12 +40,85 @@ namespace Business.Concrete
             return new SuccessDataResult<Hotel>(_hotelDal.Get(c => c.Stars == stars));
         }
 
-        public IDataResult<Hotel> GetById(int id)
+        public IDataResult<Hotel> GetByName(string name)
         {
-            return new SuccessDataResult<Hotel>(_hotelDal.Get(c => c.Id == id));
+            return new SuccessDataResult<Hotel>(_hotelDal.Get(c => c.Name.Contains(name)));
+        }
+        //valid
+        public IResult GetDbFile(IFormFile file)
+        {
+            try
+            {
+                string DbExtension = Path.GetExtension(file.FileName);
+                string newFile = file.FileName.ToLower();
+                string DbFolder = Path.Combine(directory, "DbFiles");
+                string DbPath = Path.Combine(DbFolder, newFile);
+
+                if (!Directory.Exists(DbFolder))
+                    Directory.CreateDirectory(DbFolder);
+
+                using (FileStream fileStream = File.Create(DbPath))
+                {
+                    file.CopyTo(fileStream);
+                    fileStream.Flush();
+                }
+
+                List<Hotel> records;
+                using (var reader = new StreamReader(DbPath))
+                {
+                    var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+                    {
+                        Delimiter = ",",
+                        HasHeaderRecord = true
+                    };
+
+                    using (var csv = new CsvReader(reader, csvConfig))
+                    {
+                        csv.Context.RegisterClassMap<HotelMap>();
+                        records = csv.GetRecords<Hotel>().ToList();
+
+                    }
+                }
+
+                //TODO: validation rule buraya eklenebiliyorsa hotelDal kullan yoksa this olanı kullan.
+                
+                _hotelDal.AddRange(records);
+                //this.AddRange(records);
+
+                return new Result(true, "Successful");
+            }
+            catch (Exception ex)
+            {
+                return new Result(false, ex.Message);
+            }
+
+        }
+
+        public IResult WriteAsJson(string path, List<Hotel> hotels)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult WriteAsXml(string path, List<Hotel> hotels)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Add(Hotel hotel)
+        {
+            _hotelDal.Add(hotel);
+            return new SuccessResult(Messages.HotelList);
+        }
+
+
+
+        public IResult AddRange(IEnumerable<Hotel> hotel)
+        {
+            _hotelDal.AddRange(hotel);
+            return new SuccessResult(Messages.HotelList);
         }
 
         //
-        
+
     }
 }
